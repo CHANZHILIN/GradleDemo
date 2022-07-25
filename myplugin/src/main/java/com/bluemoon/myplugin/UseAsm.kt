9aba306a9a879fe3bbf864b50de9d6256f2da643
CHANZHILIN <1181785848@qq.com>
2022-07-25 14:07:35 +0800

    delete plugin

--- myplugin/src/main/java/com/bluemoon/myplugin/UseAsm.kt
+++ myplugin/src/main/java/com/bluemoon/myplugin/UseAsm.kt
@@ -1,6 +1,9 @@
 package com.bluemoon.myplugin
 
+import groovy.lang.Closure
+import org.gradle.api.Action
 import org.gradle.api.Project
+import org.gradle.util.internal.ConfigureUtil
 
 /**
  * 扩展配置类
@@ -10,12 +13,24 @@ import org.gradle.api.Project
 open class UseAsm {
     var useAsmToApplication: Boolean = false
 
+    var useTryCatch: UseTryCatch = UseTryCatch()
+
+    // 嵌套扩展闭包函数，方法名为 useTryCatch（方法名不一定需要与属性名一致）
+    fun useTryCatch(action: Action<UseTryCatch>) {
+        action.execute(useTryCatch)
+    }
+    // 嵌套扩展闭包函数，方法名为 useTryCatch
+    fun useTryCatch(closure: Closure<UseTryCatch>) {
+        ConfigureUtil.configure(closure, useTryCatch)
+    }
+
+
     companion object {
         // 将获取扩展对象的代码封装为静态方法
         fun getConfig(project: Project): UseAsm {
             // 从 ExtensionContainer 容器获取扩展对象
             var extension = project.extensions.findByType(UseAsm::class.java)
-            println(">>>CHEN>>> Config:project=${project.rootProject.name},extensionsList=${project.extensions},extension=${extension?.useAsmToApplication}")
+            println(">>>CHEN>>> Config:project=${project.name},useAsmToApplication=${extension?.useAsmToApplication},uploadToService=${extension?.useTryCatch?.uploadToService}")
             // 配置缺省的时候，赋予默认值
             if (null == extension) {
                 extension = UseAsm()
---
+++ myplugin/src/main/java/com/bluemoon/myplugin/UseTryCatch.kt
@@ -0,0 +1,10 @@
+package com.bluemoon.myplugin
+
+/**
+ * Created by CHEN on 2022/7/25.
+ */
+class UseTryCatch {
+
+    var uploadToService: Boolean = false
+
+}
